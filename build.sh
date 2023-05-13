6d230911d63fcb897162137b34756cf5e09a63c1
afilatov <filatovaur@gmail.com>
2023-05-13 21:07:45 +0700

    minted package

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 
 rm -f presentation.aux presentation.nav presentation.toc presentation.log presentation.out presentation.snm presentation.vrb presentation.pdf
 
-pdflatex -interaction=batchmode presentation.tex
-pdflatex -interaction=batchmode presentation.tex
+pdflatex -shell-escape -interaction=batchmode presentation.tex
+pdflatex -shell-escape -interaction=batchmode presentation.tex
 
 rm -f presentation.aux presentation.nav presentation.toc presentation.log presentation.out presentation.snm presentation.vrb
 
